51dd601ab2f4450c347bc37277e8f8769eebef72
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-12 00:02:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  ii = 2 
+  if num < 2 
+    return false 
+  end
+  while (ii < num)
+    if num%ii == 0 
+      return false
+    end
+    ii +=1 
+  end
+  return true
+end
